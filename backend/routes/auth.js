b0314cb53e781f612e2c9d5c38f8d79ccc865893
sonia <teuemail@gmail.com>
2026-07-08 18:10:50 +0100

    replace bcrypt with bcryptjs for render

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 import express from "express";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import crypto from "crypto";
 import nodemailer from "nodemailer";
 import User from "../models/User.js";
